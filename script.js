b0e9fee8c1d634d4c77a08659815def9b5ef76b6
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-23 19:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// Wait for the DOM to be fully loaded before writing the string
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the body element
+  var body = document.querySelector("body");
+
+  // Check if the body has any child nodes
+  if (body.hasChildNodes()) {
+    // If there are child nodes, remove them
+    body.innerHTML = "";
+  }
+
+  // Create a new text node with the "DOM load success" string
+  var textNode = document.createTextNode("DOM load success");
+
+  // Append the text node to the body element
+  body.appendChild(textNode);
+});
